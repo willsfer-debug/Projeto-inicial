029a06dd714cb3eab78220bf5dd86fa0450bac9e
William Ferreira <willsfer@gmail.com>
2026-09-14 00:43:10 -0300

    Tela cadastro como tela inicial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main{
+    
+    public static void main (String[] args){
+         java.awt.EventQueue.invokeLater(() -> {
+            new cadastroVIEW().setVisible(true);
+        });
+
+    }
+    
+}
